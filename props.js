4de7cdf60c67f9686965dc814c2834774b328b29
adambomb00 <37837531+adambomb00@users.noreply.github.com>
2020-10-22 11:20:59 -0700

    Add files via upload

---
+++ props.js
@@ -0,0 +1,127 @@
+
+
+var data = {
+  "props": [
+    {
+      "Name": "Tax/Taxes mentioned:",
+      "A": "<50 times",
+      "B": ">50 times"
+    },
+    {
+      "Name": "Biden says:",
+      "A": "Scranton",
+      "B": "Come On Man"
+    },
+    {
+      "Name": "Biden says:",
+      "A": "Malarkey",
+      "B": "Inshallah"
+    },
+    {
+      "Name": "Trump says:",
+      "A": "Sleepy Joe",
+      "B": "Phoney Kamala"
+    },
+    {
+      "Name": "Trump says:",
+      "A": "Fake News",
+      "B": "China Virus"
+    },
+    {
+      "Name": "Either candidate says:",
+      "A": "Affordable Care Act",
+      "B": "Pre-Existing"
+    },
+    {
+      "Name": "Which event will have a higher Nielsen rating:",
+      "A": "First Debate (9/28)",
+      "B": "Second Debate (10/22)"
+    },
+    {
+      "Name": "Either mentions:",
+      "A": "Pennsylvania",
+      "B": "Florida"
+    },
+    {
+      "Name": "Either mentions:",
+      "A": "Michigan",
+      "B": "Arizona"
+    },
+    {
+      "Name": "Either mentions:",
+      "A": "North Carolina",
+      "B": "Wisconsin"
+    },
+    {
+      "Name": "Either mentions:",
+      "A": "Georgia",
+      "B": "Minnesota"
+    },
+    {
+      "Name": "Debate topic:",
+      "A": "Fighting COVID-19",
+      "B": "American Families"
+    },
+    {
+      "Name": "Debate topic:",
+      "A": "National Security",
+      "B": "Climate Change"
+    },
+    {
+      "Name": "Debate Topic",
+      "A": "Race in America",
+      "B": "Leadership"
+    },
+    {
+      "Name": "Who will take the first sip of water:",
+      "A": "Joe Biden",
+      "B": "Donald Trump"
+    },
+    {
+      "Name": "Who takes the most water sips:",
+      "A": "Donald Trump",
+      "B": "Joe Biden"
+    },
+    {
+      "Name": "Whose microphone will be muted first:",
+      "A": "Donald Trump",
+      "B": "Joe Biden"
+    },
+    {
+      "Name": "Trump Mentions Joe Rogan:",
+      "A": "Yes",
+      "B": "No"
+    },
+    {
+      "Name": "Anyone mentions Hong Kong during the debate:",
+      "A": "Yes",
+      "B": "No"
+    },
+    {
+      "Name": "First Lady and Donald Trump share an embrace after the debate:",
+      "A": "Yes",
+      "B": "No"
+    },
+    {
+      "Name": "Either Trump or Biden curse on-air:",
+      "A": "Yes",
+      "B": "No"
+    },
+    {
+      "Name": "Either candidate mentions Twitter:",
+      "A": "Yes",
+      "B": "No"
+    },
+    {
+      "Name": "Armenia-Azerbaijan conflict mentioned:",
+      "A": "Yes",
+      "B": "No"
+    },
+    {
+      "Name": "Breaks Agreed Debate rules:",
+      "A": "Trump",
+      "B": "Biden"
+    },
+  ]
+}
+
